fb67439d3eb7425926f5c967c1f512e3e0993f9a
Allan de Miranda Silva <allandemiranda@gmail.com>
2018-07-17 15:12:10 -0300

    página de editar usuário

--- sistema/index.php
+++ sistema/index.php
@@ -1,95 +1,95 @@
 ﻿<?php
     include 'cabecalho.php';
 ?>
-                <div class="inner-block">
-                    <!--market updates updates-->
-                    <div class="market-updates">
-                        <div class="col-md-4 market-update-gd">
-                            <div class="market-update-block clr-block-1">
-                                <div class="col-md-8 market-update-left">
-                                    <h3>A1</h3>
-                                    <h4>Artigo científico</h4>
-                                    <p>PVB14325-2017 - REMOÇÃO DE INCRUSTAÇÕES CARBONÁTICAS EM OLEODUTOS POR MICROEMULSÕES ÁCIDAS - ALCIDES DE OLIVEIRA WANDERLEY NETO</p>
-                                </div>
-                                <div class="col-md-4 market-update-right">
-                                    <a href="#"><i class="fa fa-file-text-o"> </i></a>
-                                </div>
-                                <div class="clearfix"> </div>
-                            </div>
-                        </div>
-                        <div class="col-md-4 market-update-gd">
-                            <div class="market-update-block clr-block-1">
-                                <div class="col-md-8 market-update-left">
-                                    <h3>A1</h3>
-                                    <h4>Artigo científico</h4>
-                                    <p>PVB14325-2017 - REMOÇÃO DE INCRUSTAÇÕES CARBONÁTICAS EM OLEODUTOS POR MICROEMULSÕES ÁCIDAS - ALCIDES DE OLIVEIRA WANDERLEY NETO</p>
-                                </div>
-                                <div class="col-md-4 market-update-right">
-                                    <a href="#"><i class="fa fa-file-text-o"> </i></a>
-                                </div>
-                                <div class="clearfix"> </div>
-                            </div>
-                        </div>
-                        <div class="col-md-4 market-update-gd">
-                            <div class="market-update-block clr-block-1">
-                                <div class="col-md-8 market-update-left">
-                                    <h3>A1</h3>
-                                    <h4>Artigo científico</h4>
-                                    <p>PVB14325-2017 - REMOÇÃO DE INCRUSTAÇÕES CARBONÁTICAS EM OLEODUTOS POR MICROEMULSÕES ÁCIDAS - ALCIDES DE OLIVEIRA WANDERLEY NETO</p>
-                                </div>
-                                <div class="col-md-4 market-update-right">
-                                    <a href="#"><i class="fa fa-file-text-o"> </i></a>
-                                </div>
-                                <div class="clearfix"> </div>
-                            </div>
-                        </div>
-                        <div class="clearfix"> </div>
-                    </div>
-                    <br />
-                    <!--market updates updates-->
-                    <div class="market-updates">
-                        <div class="col-md-4 market-update-gd">
-                            <div class="market-update-block clr-block-1">
-                                <div class="col-md-8 market-update-left">
-                                    <h3>A1</h3>
-                                    <h4>Artigo científico</h4>
-                                    <p>PVB14325-2017 - REMOÇÃO DE INCRUSTAÇÕES CARBONÁTICAS EM OLEODUTOS POR MICROEMULSÕES ÁCIDAS - ALCIDES DE OLIVEIRA WANDERLEY NETO</p>
-                                </div>
-                                <div class="col-md-4 market-update-right">
-                                    <a href="#"><i class="fa fa-file-text-o"> </i></a>
-                                </div>
-                                <div class="clearfix"> </div>
-                            </div>
-                        </div>
-                        <div class="col-md-4 market-update-gd">
-                            <div class="market-update-block clr-block-1">
-                                <div class="col-md-8 market-update-left">
-                                    <h3>A1</h3>
-                                    <h4>Artigo científico</h4>
-                                    <p>PVB14325-2017 - REMOÇÃO DE INCRUSTAÇÕES CARBONÁTICAS EM OLEODUTOS POR MICROEMULSÕES ÁCIDAS - ALCIDES DE OLIVEIRA WANDERLEY NETO</p>
-                                </div>
-                                <div class="col-md-4 market-update-right">
-                                    <a href="#"><i class="fa fa-file-text-o"> </i></a>
-                                </div>
-                                <div class="clearfix"> </div>
-                            </div>
-                        </div>
-                        <div class="col-md-4 market-update-gd">
-                            <div class="market-update-block clr-block-1">
-                                <div class="col-md-8 market-update-left">
-                                    <h3>A1</h3>
-                                    <h4>Artigo científico</h4>
-                                    <p>PVB14325-2017 - REMOÇÃO DE INCRUSTAÇÕES CARBONÁTICAS EM OLEODUTOS POR MICROEMULSÕES ÁCIDAS - ALCIDES DE OLIVEIRA WANDERLEY NETO</p>
-                                </div>
-                                <div class="col-md-4 market-update-right">
-                                    <a href="#"><i class="fa fa-file-text-o"> </i></a>
-                                </div>
-                                <div class="clearfix"> </div>
-                            </div>
-                        </div>
-                        <div class="clearfix"> </div>
-                    </div>
-                    <br />
+<div class="inner-block">
+    <!--market updates updates-->
+    <div class="market-updates">
+        <div class="col-md-4 market-update-gd">
+            <div class="market-update-block clr-block-1">
+                <div class="col-md-8 market-update-left">
+                    <h3>A1</h3>
+                    <h4>Artigo científico</h4>
+                    <p>PVB14325-2017 - REMOÇÃO DE INCRUSTAÇÕES CARBONÁTICAS EM OLEODUTOS POR MICROEMULSÕES ÁCIDAS - ALCIDES DE OLIVEIRA WANDERLEY NETO</p>
+                </div>
+                <div class="col-md-4 market-update-right">
+                    <a href="#"><i class="fa fa-file-text-o"> </i></a>
+                </div>
+                <div class="clearfix"> </div>
+            </div>
+        </div>
+        <div class="col-md-4 market-update-gd">
+            <div class="market-update-block clr-block-1">
+                <div class="col-md-8 market-update-left">
+                    <h3>A1</h3>
+                    <h4>Artigo científico</h4>
+                    <p>PVB14325-2017 - REMOÇÃO DE INCRUSTAÇÕES CARBONÁTICAS EM OLEODUTOS POR MICROEMULSÕES ÁCIDAS - ALCIDES DE OLIVEIRA WANDERLEY NETO</p>
+                </div>
+                <div class="col-md-4 market-update-right">
+                    <a href="#"><i class="fa fa-file-text-o"> </i></a>
+                </div>
+                <div class="clearfix"> </div>
+            </div>
+        </div>
+        <div class="col-md-4 market-update-gd">
+            <div class="market-update-block clr-block-1">
+                <div class="col-md-8 market-update-left">
+                    <h3>A1</h3>
+                    <h4>Artigo científico</h4>
+                    <p>PVB14325-2017 - REMOÇÃO DE INCRUSTAÇÕES CARBONÁTICAS EM OLEODUTOS POR MICROEMULSÕES ÁCIDAS - ALCIDES DE OLIVEIRA WANDERLEY NETO</p>
+                </div>
+                <div class="col-md-4 market-update-right">
+                    <a href="#"><i class="fa fa-file-text-o"> </i></a>
+                </div>
+                <div class="clearfix"> </div>
+            </div>
+        </div>
+        <div class="clearfix"> </div>
+    </div>
+    <br />
+    <!--market updates updates-->
+    <div class="market-updates">
+        <div class="col-md-4 market-update-gd">
+            <div class="market-update-block clr-block-1">
+                <div class="col-md-8 market-update-left">
+                    <h3>A1</h3>
+                    <h4>Artigo científico</h4>
+                    <p>PVB14325-2017 - REMOÇÃO DE INCRUSTAÇÕES CARBONÁTICAS EM OLEODUTOS POR MICROEMULSÕES ÁCIDAS - ALCIDES DE OLIVEIRA WANDERLEY NETO</p>
+                </div>
+                <div class="col-md-4 market-update-right">
+                    <a href="#"><i class="fa fa-file-text-o"> </i></a>
+                </div>
+                <div class="clearfix"> </div>
+            </div>
+        </div>
+        <div class="col-md-4 market-update-gd">
+            <div class="market-update-block clr-block-1">
+                <div class="col-md-8 market-update-left">
+                    <h3>A1</h3>
+                    <h4>Artigo científico</h4>
+                    <p>PVB14325-2017 - REMOÇÃO DE INCRUSTAÇÕES CARBONÁTICAS EM OLEODUTOS POR MICROEMULSÕES ÁCIDAS - ALCIDES DE OLIVEIRA WANDERLEY NETO</p>
+                </div>
+                <div class="col-md-4 market-update-right">
+                    <a href="#"><i class="fa fa-file-text-o"> </i></a>
+                </div>
+                <div class="clearfix"> </div>
+            </div>
+        </div>
+        <div class="col-md-4 market-update-gd">
+            <div class="market-update-block clr-block-1">
+                <div class="col-md-8 market-update-left">
+                    <h3>A1</h3>
+                    <h4>Artigo científico</h4>
+                    <p>PVB14325-2017 - REMOÇÃO DE INCRUSTAÇÕES CARBONÁTICAS EM OLEODUTOS POR MICROEMULSÕES ÁCIDAS - ALCIDES DE OLIVEIRA WANDERLEY NETO</p>
+                </div>
+                <div class="col-md-4 market-update-right">
+                    <a href="#"><i class="fa fa-file-text-o"> </i></a>
+                </div>
+                <div class="clearfix"> </div>
+            </div>
+        </div>
+        <div class="clearfix"> </div>
+    </div>
+    <br />
 <?php
     include 'rodape.php';
 ?>
