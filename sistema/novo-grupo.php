0c2d3e79604a8d8b514ced678846f54490b1ac82
Allan de Miranda Silva <allandemiranda@gmail.com>
2018-07-18 11:22:26 -0300

    criação das requisições - parte 1

--- sistema/novo-grupo.php
+++ sistema/novo-grupo.php
@@ -2,20 +2,25 @@
     include 'cabecalho.php';
 ?>
 <div class="inner-block">
+    <?php
+        if(true==true){
+            echo '<h2>Grupo cadastrado com sucesso !!!</h2>';
+        }
+    ?> 
     <h3>Cadastro Novo Grupo</h3>
     <br />
     <form action="/" method="get">
         <div class="formulario_cadastro_2">
             <label>Cógido do Grupo:</label>
-            <input type="text" />
+            <input type="text" nome="codigo"/>
         </div>
         <div class="formulario_cadastro_2">
             <label>Tipo do Projeto:</label>
-            <input type="text" />
+            <input type="text" nome="tipo"/>
         </div>
         <div class="formulario_cadastro_2">
             <label>Objeto de pesquisa:</label>
-            <input type="text" style="width: 100%;"/>
+            <input type="text" style="width: 100%;" nome="objeto"/>
         </div>
         <div class="formulario_cadastro_2">
             <div class="formulario_cadastro_1 col-md-4">
@@ -23,15 +28,15 @@
                 <div>
                     <div>
                         <label>Matrícula:</label>
-                        <input type="text" style="width: 100%;" />
+                        <input type="text" style="width: 100%;" nome="matricula1"/>
                     </div>
                     <div>
                         <label>Nome:</label>
-                        <input type="text" style="width: 100%;" />
+                        <input type="text" style="width: 100%;" nome="nome1"/>
                     </div>
                     <div>
                         <label>Email:</label>
-                        <input type="email" style="width: 100%;" />
+                        <input type="email" style="width: 100%;" nome="email1"/>
                     </div>
                 </div>
             </div>
@@ -40,15 +45,15 @@
                 <div>
                     <div>
                         <label>Matrícula:</label>
-                        <input type="text" style="width: 100%;" />
+                        <input type="text" style="width: 100%;" nome="matricula2"/>
                     </div>
                     <div>
                         <label>Nome:</label>
-                        <input type="text" style="width: 100%;" />
+                        <input type="text" style="width: 100%;" nome="nome2"/>
                     </div>
                     <div>
                         <label>Email:</label>
-                        <input type="email" style="width: 100%;" />
+                        <input type="email" style="width: 100%;" nome="email2"/>
                     </div>
                 </div>
             </div>
@@ -57,15 +62,15 @@
                 <div>
                     <div>
                         <label>Matrícula:</label>
-                        <input type="text" style="width: 100%;" />
+                        <input type="text" style="width: 100%;" nome="matricula3"/>
                     </div>
                     <div>
                         <label>Nome:</label>
-                        <input type="text" style="width: 100%;" />
+                        <input type="text" style="width: 100%;" nome="nome3"/>
                     </div>
                     <div>
                         <label>Email:</label>
-                        <input type="email" style="width: 100%;" />
+                        <input type="email" style="width: 100%;" nome="email3"/>
                     </div>
                 </div>
             </div>
@@ -74,15 +79,15 @@
                 <div>
                     <div>
                         <label>Matrícula:</label>
-                        <input type="text" style="width: 100%;" />
+                        <input type="text" style="width: 100%;" nome="matricula4"/>
                     </div>
                     <div>
                         <label>Nome:</label>
-                        <input type="text" style="width: 100%;" />
+                        <input type="text" style="width: 100%;" nome="nome4"/>
                     </div>
                     <div>
                         <label>Email:</label>
-                        <input type="email" style="width: 100%;" />
+                        <input type="email" style="width: 100%;" nome="email4"/>
                     </div>
                 </div>
             </div>
@@ -91,15 +96,15 @@
                 <div>
                     <div>
                         <label>Matrícula:</label>
-                        <input type="text" style="width: 100%;" />
+                        <input type="text" style="width: 100%;" nome="matricula5"/>
                     </div>
                     <div>
                         <label>Nome:</label>
-                        <input type="text" style="width: 100%;" />
+                        <input type="text" style="width: 100%;" nome="nome5"/>
                     </div>
                     <div>
                         <label>Email:</label>
-                        <input type="email" style="width: 100%;" />
+                        <input type="email" style="width: 100%;" nome="email5"/>
                     </div>
                 </div>
             </div>
@@ -108,15 +113,15 @@
                 <div>
                     <div>
                         <label>Matrícula:</label>
-                        <input type="text" style="width: 100%;" />
+                        <input type="text" style="width: 100%;" nome="matricula6"/>
                     </div>
                     <div>
                         <label>Nome:</label>
-                        <input type="text" style="width: 100%;" />
+                        <input type="text" style="width: 100%;" nome="nome6"/>
                     </div>
                     <div>
                         <label>Email:</label>
-                        <input type="email" style="width: 100%;" />
+                        <input type="email" style="width: 100%;" nome="email6"/>
                     </div>
                 </div>
             </div>
@@ -125,15 +130,15 @@
                 <div>
                     <div>
                         <label>Matrícula:</label>
-                        <input type="text" style="width: 100%;" />
+                        <input type="text" style="width: 100%;" nome="matricula7"/>
                     </div>
                     <div>
                         <label>Nome:</label>
-                        <input type="text" style="width: 100%;" />
+                        <input type="text" style="width: 100%;" nome="nome7"/>
                     </div>
                     <div>
                         <label>Email:</label>
-                        <input type="email" style="width: 100%;" />
+                        <input type="email" style="width: 100%;" nome="email7"/>
                     </div>
                 </div>
             </div>
@@ -142,15 +147,15 @@
                 <div>
                     <div>
                         <label>Matrícula:</label>
-                        <input type="text" style="width: 100%;" />
+                        <input type="text" style="width: 100%;" nome="matricula8"/>
                     </div>
                     <div>
                         <label>Nome:</label>
-                        <input type="text" style="width: 100%;" />
+                        <input type="text" style="width: 100%;" nome="nome8"/>
                     </div>
                     <div>
                         <label>Email:</label>
-                        <input type="email" style="width: 100%;" />
+                        <input type="email" style="width: 100%;" nome="email8"/>
                     </div>
                 </div>
             </div>
@@ -159,15 +164,15 @@
                 <div>
                     <div>
                         <label>Matrícula:</label>
-                        <input type="text" style="width: 100%;" />
+                        <input type="text" style="width: 100%;" nome="matricula9"/>
                     </div>
                     <div>
                         <label>Nome:</label>
-                        <input type="text" style="width: 100%;" />
+                        <input type="text" style="width: 100%;" nome="nome9"/>
                     </div>
                     <div>
                         <label>Email:</label>
-                        <input type="email" style="width: 100%;" />
+                        <input type="email" style="width: 100%;" nome="email9"/>
                     </div>
                 </div>
             </div>
--- sistema/novo-usuario.php
+++ sistema/novo-usuario.php
@@ -2,22 +2,27 @@
     include 'cabecalho.php';
 ?>
 <div class="inner-block">
+    <?php
+        if(true==true){
+            echo '<h2>Usuário cadastrado com sucesso !!!</h2>';
+        }
+    ?>    
     <h3>Cadastro Novo Usuário</h3>
     <br />
-    <form action="/" method="get">
+    <form action="novo-usuario.php" method="get">
         <div class="formulario_cadastro_2">
             <label>Primeiro Nome:</label>
-            <input type="text" />
+            <input type="text" name="nome"/>
         </div>
         <div class="formulario_cadastro_2">
             <label>Email:</label>
-            <input type="email" />
+            <input type="email" name="usuario"/>
         </div>
         <div class="formulario_cadastro_2">
             <label>Tipo de Conta:</label>
             <br />
-            <input type="radio" value="Aluno" checked/> <i>Aluno</i> <br />
-            <input type="radio" value="Professor" /> <i>Professor</i>
+            <input type="radio" name="tipo" value="Aluno" checked/> <i>Aluno</i> <br />
+            <input type="radio" name="tipo" value="Professor" /> <i>Professor</i>
         </div>                        
         <div class="hvr-fade">
             <input type="submit" value="Cadastrar" />
