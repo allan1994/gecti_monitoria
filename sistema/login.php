a77823ae675477007c2aa188d56aa6ed157cb00a
Allan de Miranda Silva <allandemiranda@gmail.com>
2018-07-18 10:35:26 -0300

    configurações de logout

--- sistema/login.php
+++ sistema/login.php
@@ -1,3 +1,7 @@
+<?php
+    unset($_SESSION['usuarioID'], $_SESSION['usuarioNome'], $_SESSION['usuarioLogin'], $_SESSION['usuarioSenha'], $_SESSION['usuarioTipo'], $_SESSION['usuarioEmail']);
+    session_destroy();
+?>
 ﻿<!DOCTYPE HTML>
 <html lang="br">
 <head>
--- sistema/logout.php
+++ sistema/logout.php
@@ -1,8 +1,6 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+unset($_SESSION['usuarioID'], $_SESSION['usuarioNome'], $_SESSION['usuarioLogin'], $_SESSION['usuarioSenha'], $_SESSION['usuarioTipo'], $_SESSION['usuarioEmail']);
+session_destroy();
+header("Location: "." ../");
+?>
