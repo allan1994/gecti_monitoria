7e804f7e379f2159692573ad0c3494918d0f941c
Allan de Miranda Silva <allandemiranda@gmail.com>
2018-07-19 19:12:42 -0300

    finalização dos grupos fechados

--- sistema/fechados.php
+++ sistema/fechados.php
@@ -1,96 +1,54 @@
 ﻿<?php
 include 'cabecalho.php';
 ?>
+<?php
+$servername = "localhost";
+$username = "gecti";
+$password = "g3cT1@(20)18";
+$dbname = "gecti";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+$sql = "SELECT * FROM grupos WHERE (responsavel='" . $_SESSION['usuarioID'] . "' AND status='Fechado')";
+$result = mysqli_query($conn, $sql);
+?>
 <div class="inner-block">
     <!--market updates updates-->
-    <div class="market-updates">
-        <div class="col-md-4 market-update-gd">
-            <div class="market-update-block clr-block-2">
-                <div class="col-md-8 market-update-left">
-                    <h3>A1</h3>
-                    <h4>Artigo científico</h4>
-                    <p>PVB14325-2017 - REMOÇÃO DE INCRUSTAÇÕES CARBONÁTICAS EM OLEODUTOS POR MICROEMULSÕES ÁCIDAS - ALCIDES DE OLIVEIRA WANDERLEY NETO</p>
-                </div>
-                <div class="col-md-4 market-update-right">
-                    <a href="#"><i class="fa fa-eye"> </i></a>
-                </div>
-                <div class="clearfix"> </div>
-            </div>
-        </div>
-        <div class="col-md-4 market-update-gd">
-            <div class="market-update-block clr-block-2">
-                <div class="col-md-8 market-update-left">
-                    <h3>A1</h3>
-                    <h4>Artigo científico</h4>
-                    <p>PVB14325-2017 - REMOÇÃO DE INCRUSTAÇÕES CARBONÁTICAS EM OLEODUTOS POR MICROEMULSÕES ÁCIDAS - ALCIDES DE OLIVEIRA WANDERLEY NETO</p>
-                </div>
-                <div class="col-md-4 market-update-right">
-                    <a href="#"><i class="fa fa-eye"> </i></a>
-                </div>
-                <div class="clearfix"> </div>
-            </div>
-        </div>
-        <div class="col-md-4 market-update-gd">
-            <div class="market-update-block clr-block-2">
-                <div class="col-md-8 market-update-left">
-                    <h3>A1</h3>
-                    <h4>Artigo científico</h4>
-                    <p>PVB14325-2017 - REMOÇÃO DE INCRUSTAÇÕES CARBONÁTICAS EM OLEODUTOS POR MICROEMULSÕES ÁCIDAS - ALCIDES DE OLIVEIRA WANDERLEY NETO</p>
-                </div>
-                <div class="col-md-4 market-update-right">
-                    <a href="#"><i class="fa fa-eye"> </i></a>
-                </div>
-                <div class="clearfix"> </div>
-            </div>
-        </div>
-        <div class="clearfix"> </div>
-    </div>
-    <br />
-    <!--market updates updates-->
-    <div class="market-updates">
-        <div class="col-md-4 market-update-gd">
-            <div class="market-update-block clr-block-2">
-                <div class="col-md-8 market-update-left">
-                    <h3>A1</h3>
-                    <h4>Artigo científico</h4>
-                    <p>PVB14325-2017 - REMOÇÃO DE INCRUSTAÇÕES CARBONÁTICAS EM OLEODUTOS POR MICROEMULSÕES ÁCIDAS - ALCIDES DE OLIVEIRA WANDERLEY NETO</p>
-                </div>
-                <div class="col-md-4 market-update-right">
-                    <a href="#"><i class="fa fa-eye"> </i></a>
-                </div>
-                <div class="clearfix"> </div>
-            </div>
-        </div>
-        <div class="col-md-4 market-update-gd">
-            <div class="market-update-block clr-block-2">
-                <div class="col-md-8 market-update-left">
-                    <h3>A1</h3>
-                    <h4>Artigo científico</h4>
-                    <p>PVB14325-2017 - REMOÇÃO DE INCRUSTAÇÕES CARBONÁTICAS EM OLEODUTOS POR MICROEMULSÕES ÁCIDAS - ALCIDES DE OLIVEIRA WANDERLEY NETO</p>
-                </div>
-                <div class="col-md-4 market-update-right">
-                    <a href="#"><i class="fa fa-eye"> </i></a>
-                </div>
-                <div class="clearfix"> </div>
-            </div>
-        </div>
-        <div class="col-md-4 market-update-gd">
-            <div class="market-update-block clr-block-2">
-                <div class="col-md-8 market-update-left">
-                    <h3>A1</h3>
-                    <h4>Artigo científico</h4>
-                    <p>PVB14325-2017 - REMOÇÃO DE INCRUSTAÇÕES CARBONÁTICAS EM OLEODUTOS POR MICROEMULSÕES ÁCIDAS - ALCIDES DE OLIVEIRA WANDERLEY NETO</p>
-                </div>
-                <div class="col-md-4 market-update-right">
-                    <a href="#"><i class="fa fa-eye"> </i></a>
-                </div>
-                <div class="clearfix"> </div>
-            </div>
-        </div>
-        <div class="clearfix"> </div>
-    </div>
-    <br />
-    <!--market updates end here-->
+    <?php
+    $contador = 1;
+    if (mysqli_num_rows($result) > 0) {
+        // output data of each row
+        while ($row = mysqli_fetch_assoc($result)) {
+            if ($contador == 1) {
+                echo '<div class="market-updates">';
+            }
+            echo '<div class="col-md-4 market-update-gd">';
+            echo '<div class="market-update-block clr-block-2">';
+            echo '<div class="col-md-8 market-update-left">';
+            echo '<h3>' . $row['codigo'] . '</h3>';
+            echo '<h4>' . $row['tipo'] . '</h4>';
+            echo '<p>' . $row['objeto'] . '</p>';
+            echo '</div>';
+            echo '<div class="col-md-4 market-update-right">';
+            echo '<a href="editar-grupo.php?idGrupo=' . $row['id'] . '"><i class="fa fa-eye"></i></a>';
+            echo '</div>';
+            echo '<div class="clearfix"> </div>';
+            echo '</div>';
+            echo '</div>';
+            if (($contador % 3) == 0) {
+                echo '</div>';
+                echo '<br />';
+            }
+            $contador++;
+        }
+    } else {
+        echo "0 results";
+    }
+    mysqli_close($conn);
+    ?>
 </div>
 <?php
 include 'rodape.php';
