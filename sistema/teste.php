433923b86ceb1ed25e2ad78344feb11fb296c779
Allan de Miranda Silva <allandemiranda@gmail.com>
2018-07-22 16:12:05 -0300

    FIM

--- sistema/teste.php
+++
@@ -1,6 +0,0 @@
-<form method="get" action="teste.php">
-    <input type="text" name="input">
-    <textarea name="areaDeTexdto"></textarea>
-    <input type="submit" name="acao" value="salvar">
-    <input type="submit" name="acao" value="sair">
-</form>
